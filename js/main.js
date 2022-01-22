75a670b4a10c117a064964be5006798c1384507c
Tina363 <79143567+Tina363@users.noreply.github.com>
2022-01-22 16:15:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// const { Scrollbar } = require("swiper");
+
+// $(function(){
+
+//   import Swiper, { Navigation
+//     // ,
+//     //  Pagination
+//     } from 'swiper';
+//   // // import Swiper and modules styles
+  
+
+//   // // configure Swiper to use modules
+//   Swiper.use([Navigation
+//     // , Pagination
+//   ]);
+
+//   const swiper = new Swiper('.swiper', {
+//     // modules: [Scrollbar],
+//     // modules: [Navigation],
+//     // Optional parameters
+//     // direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     // pagination: {
+//     //   el: '.swiper-pagination',
+//     // },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+  
+
+// // });
+// //спойлер на джейквери
+// // $(document).ready(function() {
+// //   $('.block__title').click(function(event) {
+// //       if($('.block').hasClass('one')){
+// //           $('.block__title').not($(this)).removeClass('active');
+// //           $('.block__text').not($(this).next()).slideUp(300);
+// //       }
+// //       $(this).toggleClass('active').next().slideToggle(300);
+// //   });
+
+// });
+
+// function ibg() {
+//   $.each($('.ibg'), function(index, val) {
+//     if($(this).find('.img').length>0) {
+//       $(this).css('background-image', 'url("'+$(this).find('img').attr('src')+'")');
+//     });
+    
+  
+
